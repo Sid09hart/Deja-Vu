6e0b244b282d9aa00938d8524bae3619d7b41f78
Siddharth Negi <siddharth0negi12@gmail.com>
2025-11-23 12:17:47 +0530

    Minor Changes

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,10 @@ import memeRoutes from './routes/memeRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    crendentials:true,
+}));
 app.use(express.json());
 
 // Connect to Database
@@ -22,7 +25,7 @@ app.use('/',(req,res)=>{
     
 })
 
-const PORT = process.env.FRONTEND_URL || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port  http://localhost:${PORT}`);
